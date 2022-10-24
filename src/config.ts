9cc871a8d16a30fd3d6397d70d1b89ac9daf2dac
Alec Reimel <alecreimel1@gmail.com>
2022-10-24 10:26:28 -0400

    fonts

--- src/config.ts
+++ src/config.ts
@@ -47,11 +47,11 @@ export type Sidebar = Record<
 >;
 export const SIDEBAR: Sidebar = {
 	en: {
-		'Section Header': [
+		'Section 1': [
 			{ text: 'Introduction', link: 'en/introduction' },
 			{ text: 'Page 2', link: 'en/page-2' },
 			{ text: 'Page 3', link: 'en/page-3' },
 		],
-		'Another Section': [{ text: 'Page 4', link: 'en/page-4' }],
+		'Section 2': [{ text: 'Page 4', link: 'en/page-4' }],
 	},
 };
